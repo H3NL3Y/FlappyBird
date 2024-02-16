9eb0a98136489fc5026a6ed041f928c26f2d76b7
H3NL3Y <rdhenley93@gmail.com>
2024-02-16 23:17:50 +0000

    Game Start
    Assets gathered and game started, added to github.

---
+++ Game/main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+
+var game_running : bool
+var game_over:bool # Declares the state of the game
+var scroll
+var score
+const SCROLL_SPEED : int=4 #Controls the speed of the game
+var screen_size : Vector2i
+var ground_height : int
+var pipes : Array #Able to store pipes
+const PIPE_DELAY : int = 100
+const PIPE_RANGE : int = 200
+var Bird = load("res://bird.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+	
+func new_game():
+	game_running = false
+	game_over = false
+	
+	score = 0
+	scroll = 0
+	$Bird.reset()
+	
+func _input(event):
+	if game_over == false: 
+		if event is InputEventMouseButton: 
+			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed: 
+				if game_running == false: 
+					start_game()
+				else:
+					if $Bird.flying:
+						$Bird.flap()
+
+func start_game():
+	game_running = true
+	$Bird.flying = true
+	$Bird.flap()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
